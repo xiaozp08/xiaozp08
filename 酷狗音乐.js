9488672946f2159875a5c25e13abb52642bd3b6d
xiaozp08 <358542202@qq.com>
2024-01-21 14:11:37 +0000

    替换 酷狗音乐.js

--- 酷狗音乐.js
+++ 酷狗音乐.js
@@ -342,11 +342,11 @@ async function importMusicSheet(urlLike) {
     return musicList;
 }
 module.exports = {
-    platform: "酷狗",
+    platform: "酷狗音乐",
     version: "0.1.3",
     appVersion: ">0.1.0-alpha.0",
     order: 17,
-    srcUrl: "http://adad23u.appinstall.life/dist/kugou/index.js",
+    srcUrl: "https://gitlab.com/xiaozp08/xiaoTV/-/raw/main/酷狗音乐.js",
     cacheControl: "no-cache",
     primaryKey: ["id", "album_id", "album_audio_id"],
     hints: {
