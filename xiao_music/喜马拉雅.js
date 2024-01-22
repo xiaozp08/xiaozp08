92d17e44c9454b3c0f2f37f8eb0d2b647f213c44
xiaozp08 <358542202@qq.com>
2024-01-22 13:14:27 +0000

    替换 喜马拉雅.js

--- xiao_music/喜马拉雅.js
+++ xiao_music/喜马拉雅.js
@@ -1,379 +1,173 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
-const cheerio_1 = require("cheerio");
-const CryptoJS = require("crypto-js");
-const searchRows = 20;
-const host="http://www.86109.com";
-const tuku="http://tv.zanlagua.com/tuku/"
-async function getMediaSource(musicItem, quality) {
-    
-
-    var songkey=musicItem.id.match(/\/flac\/(.*?).html/)[1]  
-    
-          const headers2 = {
-            "Accept":"application/json, text/javascript, */*; q=0.01",
-            "Accept-Encoding":"gzip, deflate",
-            "Accept-Language":"zh-CN,zh;q=0.9",
-            "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-            "X-Requested-With":"XMLHttpRequest",
-    };   
-    
-   
-    
- //  const html2 = await axios_1.get(host+songkey,headers2);   
-   // const obj=html2.data; 
-   
-    
-    var murl="http://api.86109.com/mgmp3/"+songkey+".mp3";
- 
-  console.log(murl);
-    
-        
-        
-    
-       return {
-          
-            url:murl,
-
-        };
-        
-        
+const dayjs = require("dayjs");
+const CryptoJs = require("crypto-js");
+function formatMusicItem(_) {
+    var _a, _b;
+    return {
+        id: (_a = _.id) !== null && _a !== void 0 ? _a : _.trackId,
+        artist: _.nickname,
+        title: _.title,
+        album: _.albumTitle,
+        duration: _.duration,
+        artwork: ((_b = _.coverPath) === null || _b === void 0 ? void 0 : _b.startsWith("//"))
+            ? `https:${_.coverPath}`
+            : _.coverPath,
+    };
 }
-
-  async function getMusicInfo(musicItem) {
-      console.log('getMusicInfo');
-    // 根据音乐获取音乐详细信息
-    var songkey=musicItem.id.match(/\/song\/(.*?).html/)[1]  
-    
-          const headers2 = {
-            "Accept":"application/json, text/javascript, */*; q=0.01",
-            "Accept-Encoding":"gzip, deflate",
-            "Accept-Language":"zh-CN,zh;q=0.9",
-            "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-            "X-Requested-With":"XMLHttpRequest",
-    };   
-    
-   
-    
-    const html2 = await axios_1.get(host+songkey,headers2);   
-    const obj=html2.data; 
-    console.log(host+songkey);
-    
+function formatAlbumItem(_) {
+    var _a, _b, _c;
     return {
-      artwork: obj["pic"],
-      title:obj["title"].split('-')[1],
-      artist:obj["title"].split('-')[0],
+        id: (_a = _.albumId) !== null && _a !== void 0 ? _a : _.id,
+        artist: _.nickname,
+        title: _.title,
+        artwork: ((_b = _.coverPath) === null || _b === void 0 ? void 0 : _b.startsWith("//"))
+            ? `https:${_.coverPath}`
+            : _.coverPath,
+        description: (_c = _.intro) !== null && _c !== void 0 ? _c : _.description,
+        date: _.updatedAt ? dayjs(_.updatedAt).format("YYYY-MM-DD") : null,
     };
-  }
-
-async function getRecommendSheetTags() {
-    
-    
-    
-    const headers = {
-    };
-    const params = {
-    };
-      
-       const html = await axios_1.get(host+"/playtype/style.html", { headers, params });
-              const obj=html.data;    
-              const $ = (0, cheerio_1.load)(obj);  
-              const rawAlbums2 = $("div.main").find("div.class");
-              const albums2 = [];   
-
-          var arr1=[];    
-        for(let i=0;i<rawAlbums2.length;i++){
-                const plp=$(rawAlbums2[i]).find("h1")[0];
-                const plp2=$(rawAlbums2[i]).find("li");
-                var arr2=[];
-                
-                for(let j=1;j<plp2.length;j++){
-                        const jlj=plp2[j];
-                        arr2.push({id:$(jlj).find("a").attr("href"),title:$(jlj).text()})
-                }
-                arr1.push({title:$(plp).text(),data:arr2});
-        }
-        
-        
-
-        
-  
-    // 获取推荐歌单 tag
+}
+function formatArtistItem(_) {
     return {
-      pinned: [{"id":"/playtype/index.html","title":"歌单"}],
-      data: arr1,
+        name: _.nickname,
+        id: _.uid,
+        fans: _.followersCount,
+        description: _.description,
+        avatar: _.logoPic,
+        worksNum: _.tracksCount,
     };
-  }
-async function getRecommendSheetsByTag(tagItem,page) {
-    // 获取某个 tag 下的所有歌单
-    console.log('getRecommendSheetsByTag');
-         const headers = {
-
+}
+function paidAlbumFilter(raw) {
+    var _a;
+    return !((_a = raw.priceTypes) === null || _a === void 0 ? void 0 : _a.length);
+}
+function paidMusicFilter(raw) {
+    return raw.tag === 0 || raw.isPaid === false || parseFloat(raw.price) === 0;
+}
+async function searchBase(query, page, core) {
+    return (await axios_1.default.get("https://www.ximalaya.com/revision/search/main", {
+        params: {
+            kw: query,
+            page: page,
+            spellchecker: true,
+            condition: "relation",
+            rows: 20,
+            device: "iPhone",
+            core,
+            paidFilter: true,
+        },
+    })).data;
+}
+async function searchMusic(query, page) {
+    const res = (await searchBase(query, page, "track")).data.track;
+    return {
+        isEnd: page >= res.totalPage,
+        data: res.docs.filter(paidMusicFilter).map(formatMusicItem),
     };
-    const params = {
-
+}
+async function searchAlbum(query, page) {
+    const res = (await searchBase(query, page, "album")).data.album;
+    return {
+        isEnd: page >= res.totalPage,
+        data: res.docs.filter(paidAlbumFilter).map(formatAlbumItem),
     };
-    if(page==""){page=1;}
-    if(page<=0){page=1;}
-    var isends=false;
-    
-    if(tagItem.id==""){
-              const html = await axios_1.get(host+"/playtype/hot/"+page+".html", { headers, params });
-              const obj=html.data;    
-              const $ = (0, cheerio_1.load)(obj);  
-              const rawAlbums2 = $("div.video_list").find("li");
-              const albums2 = [];   
-              if(rawAlbums2.length<31){
-                  isends=true;
-              }
-               
-               for(let i=0;i<rawAlbums2.length;i++){
-                   
-                         var imgcc=$(rawAlbums2[i]).find("a").find("img").attr("src");
-                  
-                          if(imgcc.substring(0,4)=="http"){}else{
-                                imgcc=host+imgcc;
-                            }  
-                          
-                             
-                                 albums2.push({
-                                        id: $(rawAlbums2[i]).find("a").attr("href"),
-                                        title: $(rawAlbums2[i]).find("a").text(),
-                                        artwork:imgcc,
-                                       
-                                    });
-                 
-               }
-        
-        
-    }else  if(tagItem.id!=""){
-        
-           var curl=tagItem.id;
-           var lpo=curl.replace(".html","");
-           
-           
-           var ourl="";
-           if(page>1){
-               var ourl=host+lpo+"/"+page+".html";
-           }else{
-               var ourl=host+curl;
-           }
-           
-              const html = await axios_1.get(ourl, { headers, params });
-              const obj=html.data;    
-              
-              const $ = (0, cheerio_1.load)(obj);  
-              const rawAlbums2 = $("div.singer_list").find("li");
-              const maxn=96;
-              if(rawAlbums2.length==0){rawAlbums2 = $("div.video_list").find("li");maxn=30}
-              
-              const albums2 = []; 
-              
-              if(rawAlbums2.length<maxn){
-                  isends=true;
-              }
-               
-               for(let i=0;i<rawAlbums2.length;i++){
-                   
-                         var imgcc=$(rawAlbums2[i]).find("a").find("img").attr("src");
-                  
-                          if(imgcc.substring(0,4)=="http"){}else{
-                                imgcc=host+imgcc;
-                            }  
-                          
-                             
-                                 albums2.push({
-                                        id: $(rawAlbums2[i]).find("a").attr("href"),
-                                        title: $(rawAlbums2[i]).find("a").text(),
-                                        artwork:imgcc,
-                                        
-                                    });
-               }
+}
+async function searchArtist(query, page) {
+    const res = (await searchBase(query, page, "user")).data.user;
+    return {
+        isEnd: page >= res.totalPage,
+        data: res.docs.map(formatArtistItem),
+    };
+}
+async function getAlbumInfo(albumItem, page = 1) {
+    const res = await axios_1.default.get("https://www.ximalaya.com/revision/album/v1/getTracksList", {
+        params: {
+            albumId: albumItem.id,
+            pageNum: page,
+            pageSize: 50,
+        },
+    });
+    return {
+        isEnd: page * 50 >= res.data.data.trackTotalCount,
+        albumItem: {
+            worksNum: res.data.data.trackTotalCount,
+        },
+        musicList: res.data.data.tracks.filter(paidMusicFilter).map((_) => {
+            const r = formatMusicItem(_);
+            r.artwork = albumItem.artwork;
+            r.artist = albumItem.artist;
+            return r;
+        }),
+    };
+}
+async function search(query, page, type) {
+    if (type === "music") {
+        return searchMusic(query, page);
     }
-    return {
-      isEnd: isends,
-      data: albums2,
-    };
-}
-async function getMusicSheetInfo(sheetItem, page) {
-      console.log('getMusicSheetInfo');
-            const headers = {
-                
-            };
-            const params = {
-        
-            };
-            
-         if(sheetItem.id.match(/\/singer\/(.*?).html/))   {
-            var userkey=sheetItem.id.match(/\/singer\/(.*?).html/)[1];
-            var plp="/singer/";
-            var curl=host+plp+userkey+"/"+page+".html";
-         }else{
-            var userkey=sheetItem.id.match(/\/playlist\/(.*?).html/)[1];
-            var plp="/playlist/";
-            var curl=host+plp+userkey+"/"+page+".html";
-         }
-        console.log(curl);
-            const albums = [];
-            const html = await axios_1.default.get(curl, {headers,params});
-            const obj=html.data;
-           
-               
-            const $ = (0, cheerio_1.load)(obj);
-            
-            const pimg = $("div.pic").find("img").attr("src");
-            const pname = $("div.list_r").find("h1").text();
-            const pabout = $("div.info").text();
-   
-            
-            const rawAlbums = $("div.play_list").find("li");
-            console.log(pabout);
-             
-             
-             for(let i=0;i<rawAlbums.length;i++){
-                 albums.push({
-                              platform: '86109',
-                              id: $(rawAlbums[i]).find("a").attr("href"),
-                              artist:$(rawAlbums[i]).find("a.singer").text(),
-                              title: $(rawAlbums[i]).find("a").text(),
-                              album: "86109",
-                              artwork:$(rawAlbums[i]).find("img").attr("src"),
-                              target2:$(rawAlbums[i]).find("a").attr("target"),
-    
-                 });
-             }
-             
-             
-             
-        var iendpage=false;
-        if(rawAlbums.length<32){iendpage=true;}
-
-        if(page<=1){
-              return {
-                isEnd: iendpage,
-                musicList: albums,
-                albumItem: {
-                  description:pabout,
-                },
-              }
-        }else{
-               return {
-                isEnd: iendpage,
-                musicList: albums,
-
-              }
-        }
-    
-    
-}
-
-  
-   async function getLyric(musicItem) {
-       var songkey=musicItem.id.match(/\/flac\/(.*?).html/)[1]  
-    return {
-      lrc: host+"/plug/down.php?ac=lrc&id="+songkey, // 链接
-      
-    };
-  } 
-  
-  async function search(query, page, type) {
-      console.log("search");
-                  const headers = {
-                
-            };
-            const params = {
-        
-            };  
-   if (type === "music") {
-      var curl=host+"/so.php?wd="+query+"&page="+page;
-      
-      
-      
-      
-       const albums = [];
-            const html = await axios_1.default.get(curl, {headers,params});
-            const obj=html.data;
-           
-               
-            const $ = (0, cheerio_1.load)(obj);
-            
-            const pimg = $("div.pic").find("img").attr("src");
-            const pname = $("div.list_r").find("h1").text();
-            const pabout = $("div.info").find("p").text();
-   
-            
-            const rawAlbums = $("div.play_list").find("li");
-            
-             
-             
-             for(let i=0;i<rawAlbums.length;i++){
-                 albums.push({
-                              platform: '86109',
-                              id: $(rawAlbums[i]).find("a.url").attr("href"),
-                              artist:$(rawAlbums[i]).find("a.singer").text(),
-                              title: $(rawAlbums[i]).find("a.url").text(),
-                              album: "86109",
-                              artwork:$(rawAlbums[i]).find("img").attr("src"),
-                              target2:$(rawAlbums[i]).find("a").attr("target"),
-    
-                 });
-             }
-             
-             
-             
-        var iendpage=false;
-        if(rawAlbums.length<68){iendpage=true;}
-      
-      
-      return {
-        isEnd: iendpage,
-        data: albums, // MusicItem 类型的数组
-      };
-    }
-    // 搜索专辑
     else if (type === "album") {
-      return {
-        isEnd: true,
-        data: [], // AlbumItem 类型的数组
-      };
+        return searchAlbum(query, page);
     }
-    // 搜索歌单
-    else if (type === "sheet") {
-      return {
-        isEnd: true,
-        data: [], // MusicSheetItem 类型的数组
-      };
-    }
-    // 搜索作者
     else if (type === "artist") {
-      return {
-        isEnd: true,
-        data: [], // ArtistItem 类型的数组
-      };
+        return searchArtist(query, page);
     }
-  }
+}
+async function getMediaSource(musicItem, quality) {
+    if (quality !== "standard") {
+        return;
+    }
+    const info = (await axios_1.default.get(`https://www.ximalaya.com/mobile-playpage/track/v3/baseInfo/${Date.now()}?device=www&trackId=${musicItem.id}&trackQualityLevel=1`)).data;
+    const trackInfo = info.trackInfo;
+    const { playUrlList } = trackInfo;
+    const encodeText = playUrlList[0].url;
+    const url = CryptoJs.AES.decrypt({
+        ciphertext: CryptoJs.enc.Base64url.parse(encodeText),
+    }, CryptoJs.enc.Hex.parse("aaad3e4fd540b0f79dca95606e72bf93"), {
+        mode: CryptoJs.mode.ECB,
+        padding: CryptoJs.pad.Pkcs7,
+    }).toString(CryptoJs.enc.Utf8);
+    return {
+        url,
+    };
+}
+async function getArtistWorks(artistItem, page, type) {
+    if (type === "music") {
+        const res = (await axios_1.default.get("https://www.ximalaya.com/revision/user/track", {
+            params: {
+                page,
+                pageSize: 30,
+                uid: artistItem.id,
+            },
+        })).data.data;
+        return {
+            isEnd: res.page * res.pageSize >= res.totalCount,
+            data: res.trackList.filter(paidMusicFilter).map((_) => (Object.assign(Object.assign({}, formatMusicItem(_)), { artist: artistItem.name }))),
+        };
+    }
+    else {
+        const res = (await axios_1.default.get("https://www.ximalaya.com/revision/user/pub", {
+            params: {
+                page,
+                pageSize: 30,
+                uid: artistItem.id,
+            },
+        })).data.data;
+        return {
+            isEnd: res.page * res.pageSize >= res.totalCount,
+            data: res.albumList.filter(paidAlbumFilter).map((_) => (Object.assign(Object.assign({}, formatAlbumItem(_)), { artist: artistItem.name }))),
+        };
+    }
+}
 module.exports = {
-  /** 用来说明插件信息的属性 */
-  platform: "86109",
-  version: "0.0.0", // 插件版本号
-  hints: {
-        importMusicSheet: [
-            "86109.com采集",
-
-        ],
-    },
-    primaryKey: ["id"],supportedSearchType: ["music"],
-    cacheControl: "no-store",
-    srcUrl: "http://tv.zanlagua.com/86109.js",
-  /** 供给软件在合适的时机调用的函数 */
-
-
-  getMediaSource,
-  getMusicInfo,
-  getRecommendSheetTags,
-  getRecommendSheetsByTag,
-  getMusicSheetInfo,
-search,
-  getLyric,
+    platform: "喜马拉雅听书",
+    author: '猫头猫',
+    version: "0.1.6",
+    supportedSearchType: ["music", "album", "artist"],
+    srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/xmly/index.js",
+    cacheControl: "no-cache",
+    search,
+    getAlbumInfo,
+    getMediaSource,
+    getArtistWorks,
 };
